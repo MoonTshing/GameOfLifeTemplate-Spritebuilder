dc203b4e151a8844e5b7154cb7d1550052a54ea7
chanjuaz <tshingcj@gmail.com>
2015-01-27 19:16:11 -0500

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Source/Grid.h
+++ GameOfLife.spritebuilder/Source/Grid.h
@@ -7,23 +7,10 @@
 //
 
 #import "CCSprite.h"
-#import "Grid.h"
-#import "Creature.h"
 
 @interface Grid : CCSprite
 
 @property (nonatomic, assign) int totalAlive;
 @property (nonatomic, assign) int generation;
-
-@end
-// these are variables that cannot be changed
-static const int GRID_ROWS = 8;
-static const int GRID_COLUMNS = 10;
-
-@implementation Grid {
-    NSMutableArray *_gridArray;
-    float _cellWidth;
-    float _cellHeight;
-}
 
 @end
